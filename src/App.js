2a5ea6774ec1ea5d5bdbd177a8b78f81d98f81ac
amine620 <mohafidi2002@gmail.com>
2021-06-03 12:32:52 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import DataCenter from "./API/DataCenter";
+import Products from "./components/products/Products";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router , Route , Switch} from 'react-router-dom'
+import Details from "./components/products/details";
+import Navbar from "./components/products/Navbar";
+import Form from "./components/products/Form";
+import Update from "./components/products/Update";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataCenter>
+
+        <Router>
+          <Navbar/>
+          <Switch>
+            <Route exact path='/' component={Products} />
+            <Route exact path='/details/:id' component={Details} />
+            <Route exact path='/add' component={Form} />
+            <Route exact path='/update/:id' component={Update} />
+            <Route  />
+          </Switch>
+        </Router>
+
+      </DataCenter>
+
     </div>
   );
 }
